bdf09ae6552b79592c671cffc3e202ee7620d83c
Kaitlyn <harvey29@pdx.edu>
2026-03-12 13:19:21 -0700

    worked on contact page

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("page loaded");
   document.documentElement.style.scrollBehavior = "smooth";
 });
+
+const text = "hello, welcome to my site!";
+const typedEl = document.querySelector(".typed");
+const cursorEl = document.querySelector(".cursor");
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    typedEl.textContent += text[i];
+    i++;
+    setTimeout(type, 60);
+  } else {
+  }
+}
+
+type();
